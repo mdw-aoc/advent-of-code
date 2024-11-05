90d8a736cbdc4d1d248aa991af9aa665209c3598
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2024-11-04 21:38:07 -0700

    Beginning work on 2023 day 10 part 2 in Go.

--- go/2023/day10/main_test.go
+++ go/2023/day10/main_test.go
@@ -14,12 +14,58 @@ const TODO = -1
 
 var (
 	inputLines = slices.Collect(inputs.Read(2023, 10).Lines())
-	sampleLines = []string{
+	sampleA    = []string{ // 1 enclosed tile
 		"..F7.",
 		".FJ|.",
 		"SJ.L7",
 		"|F--J",
 		"LJ...",
+	}
+	sampleB = []string{ // 4 enclosed tiles
+		"...........",
+		".S-------7.",
+		".|F-----7|.",
+		".||.....||.",
+		".||.....||.",
+		".|L-7.F-J|.",
+		".|..|.|..|.",
+		".L--J.L--J.",
+		"...........",
+	}
+	sampleC = []string{ // 4 enclosed tiles
+		"..........",
+		".S------7.",
+		".|F----7|.",
+		".||....||.",
+		".||....||.",
+		".|L-7F-J|.",
+		".|II||II|.",
+		".L--JL--J.",
+		"..........",
+	}
+	sampleD = []string{ // 8 enclosed tiles
+		".F----7F7F7F7F-7....",
+		".|F--7||||||||FJ....",
+		".||.FJ||||||||L7....",
+		"FJL7L7LJLJ||LJ.L-7..",
+		"L--J.L7...LJS7F-7L7.",
+		"....F-J..F7FJ|L7L7L7",
+		"....L7.F7||L7|.L7L7|",
+		".....|FJLJ|FJ|F7|.LJ",
+		"....FJL-7.||.||||...",
+		"....L---J.LJ.LJLJ...",
+	}
+	sampleE = []string{ // 10 enclosed tiles
+		"FF7FSF7F7F7F7F7F---7",
+		"L|LJ||||||||||||F--J",
+		"FL-7LJLJ||||||LJL-77",
+		"F--JF--7||LJLJ7F7FJ-",
+		"L---JF-JLJ.||-FJLJJ7",
+		"|F|F-JF---7F7-L7L|7|",
+		"|FFJF7L7F-JF7|JL---7",
+		"7-L-JL7||F7|L7F-7F7|",
+		"L.L7LFJ|||||FJL7||LJ",
+		"L7JLJL-JLJLJL--JLJ.L",
 	}
 )
 
@@ -35,15 +81,18 @@ func (this *Suite) Setup() {
 }
 
 func (this *Suite) TestPart1A() {
-	this.So(this.Part1(sampleLines), should.Equal, 8)
+	this.So(this.Part1(sampleA), should.Equal, 8)
 }
 func (this *Suite) TestPart1Full() {
 	this.So(this.Part1(inputLines), should.Equal, 6886)
 }
 func (this *Suite) TestPart2A() {
-	this.So(this.Part2(sampleLines), should.Equal, TODO)
+	this.So(this.Part2(sampleB), should.Equal, 4)
+	//this.So(this.Part2(sampleC), should.Equal, 4)
+	//this.So(this.Part2(sampleD), should.Equal, 8)
+	//this.So(this.Part2(sampleE), should.Equal, 10)
 }
-func (this *Suite) TestPart2Full() {
+func (this *Suite) SkipTestPart2Full() {
 	this.So(this.Part2(inputLines), should.Equal, TODO)
 }
 func (this *Suite) Part1(lines []string) any {
