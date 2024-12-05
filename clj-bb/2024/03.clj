aba43c94c6a65fc3476ba6277570db06be1ab6bc
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2024-12-05 00:11:15 -0700

    Extract patterns.

--- clj-bb/2024/03.clj
+++ clj-bb/2024/03.clj
@@ -1,9 +1,10 @@
 (def sample-input (slurp "03-sample.txt"))
 (def actual-input (slurp "03-actual.txt"))
 
+(def expression-pattern #"mul\((\d{1,3}),(\d{1,3})\)")
 (defn parse-multiplications [input]
   (->> input
-       (re-seq #"mul\((\d{1,3}),(\d{1,3})\)")
+       (re-seq expression-pattern)
        (map rest) ; groups contain the entire match first, then the capture groups
        (flatten)
        (map #(Integer/parseInt %))
@@ -16,11 +17,12 @@
 
 (def sample-input (slurp "03-sample2.txt")) ; redefine
 
+(def do-pattern   #"do\(\)")
+(def dont-pattern #"don't\(\)")
 (defn remove-donts [input]
-  (let [dosplit   (str/split input #"do\(\)")
-        dont      #"don't\(\)"
-        dontsplit (map #(first (str/split % dont)) dosplit)]
-    (apply str dontsplit)))
+  (let [do-split   (str/split input do-pattern)
+        dont-split (map #(first (str/split % dont-pattern)) do-split)]
+    (apply str dont-split)))
 
 (println 48       (parse-multiplications (remove-donts sample-input)))
 (println 78683433 (parse-multiplications (remove-donts actual-input)))
