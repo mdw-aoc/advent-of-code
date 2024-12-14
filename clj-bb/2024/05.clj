bcc59399b4324a07da277ede6259f574d9b8055c
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2024-12-14 00:08:34 -0700

    Solve 2024 day 5 part 2 in Clojure.

--- clj-bb/2024/05.clj
+++ clj-bb/2024/05.clj
@@ -22,3 +22,32 @@
 
 (println 143  (part1 sample-input))
 (println 4814 (part1 actual-input))
+
+(defn swap-items [lst idx1 idx2]
+  (let [vector (vec lst)]
+    (assoc vector
+           idx1 (vector idx2)
+           idx2 (vector idx1))))
+
+(defn resort [rules update]
+  (loop [i 2 update update]
+    (if (is-valid? rules update)
+      update
+      (let [part (take i update)]
+        (if (is-valid? rules part)
+          (recur (inc i) update)
+          (recur 0 (swap-items update (- i 2) (- i 1))))))))
+
+(defn part2 [input]
+  (let [parsed  (parse-input input)
+        rules   (parsed :rules)
+        updates (parsed :updates)]
+    (->> updates
+         (remove (partial is-valid? rules))
+         (map (partial resort rules))
+         (map #(nth %1 (/ (count %1) 2)))
+         (map #(Integer/parseInt %))
+         (reduce +))))
+
+(println 123  (part2 sample-input))
+(println 5448 (part2 actual-input))
